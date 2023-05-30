48e3942d3997a1517542e877ec22ec477b01687f
Austin <austinodhiambo64@gmail.com>
2023-05-30 21:19:44 +0300

    Assignment Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,16 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'interactive_record.rb'
+require_relative '../lib/interactive_record.rb'
 
 class Student < InteractiveRecord
-
-end
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+  
+    def self.find_by(attributes)
+      conditions = attributes.map { |attr, value| "#{attr} = '#{value}'" }.join(" AND ")
+      sql = "SELECT * FROM #{table_name} WHERE #{conditions}"
+      DB[:conn].execute(sql)
+    end
+  end
+  
